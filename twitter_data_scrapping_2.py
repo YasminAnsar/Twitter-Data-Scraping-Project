caee1256838d975df6c3da44a949159e87877915
Ansar Yasmin <ansar.yasmin@hyperisland.se>
2022-11-29 14:23:03 +0100

    Update twitter_data_scrapping_2.py

--- twitter_data_scrapping_2.py
+++ twitter_data_scrapping_2.py
@@ -2,7 +2,7 @@
 import tweepy as tw    #to access twitter api
 import pandas as pd    # for making data frames and stroing data after retrieving
 
-#Reading file keys.py that stores all the keys 
+#Reading file keys.py that stores all the keys seprately
 from keys import *
 import requests
 
@@ -29,6 +29,6 @@ tweets_data = tweets_dict['data']
 
 #Transforming to pandas dataframe
 df = pd.json_normalize(tweets_data)
-df
+
 #saving the dataframe for analysis 
 df.to_csv("music_twitter_data.csv")
